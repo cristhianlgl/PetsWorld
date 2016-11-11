3816e6ad4c79ec85d68b6342a83c37e0a61b6f18
cristhian <cristhianlgl@hotmail.com>
2016-11-11 18:12:27 -0500

    cambios para agregar ejemplo de la API de google Maps

--- app/src/main/java/com/unad/diplomado/petsworld/ui/fragmentos/AcercadeFragment.java
+++ app/src/main/java/com/unad/diplomado/petsworld/ui/fragmentos/AcercadeFragment.java
@@ -1,13 +1,17 @@
 package com.unad.diplomado.petsworld.ui.fragmentos;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.unad.diplomado.petsworld.MainActivity;
 import com.unad.diplomado.petsworld.R;
+import com.unad.diplomado.petsworld.ui.actividades.MapsActivity;
 
 /**
  * Created by Fizz on 30/10/2016.
@@ -19,6 +23,17 @@ public class AcercadeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //returning our layout file
         View v  = inflater.inflate(R.layout.fragment_acercade, container, false);
+        Button button = (Button) v.findViewById(R.id.button_mapa);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
         return v;
     }
 
